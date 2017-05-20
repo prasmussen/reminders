9d3d3f88589283f9382b62977975dd0bef5facee
Petter Rasmussen <petter.rasmussen@gmail.com>
2017-05-20 18:00:35 +0200

    Refactor

--- web/reminders.js
+++ web/reminders.js
@@ -2,12 +2,62 @@ var app = Elm.Main.fullscreen();
 
 // Listen for getUser requests from elm
 app.ports.getUser.subscribe(function() {
-    gapi.load('client:auth2', gapiInit);
+    // Helper function to send current user to elm
+    function sendUser(auth) {
+        var user = formatUser(auth);
+        app.ports.getUserSuccess.send(user);
+    }
+
+    gapi.load('client:auth2', function() {
+        gapi.client.init({
+            'apiKey': 'AIzaSyAYk1JO0wu3UxAbYmeDxLclOA2psEwLQZE',
+            'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+            'clientId': '853474051422-rbvibfhir17apk19nq9vf3j8sshdo81f.apps.googleusercontent.com',
+            'scope': 'https://www.googleapis.com/auth/calendar',
+        }).then(function() {
+            var auth = gapi.auth2.getAuthInstance();
+
+            // Listen for sign in events from elm
+            app.ports.signIn.subscribe(function() {
+                auth.signIn();
+            });
+
+            // Listen for sign out events from elm
+            app.ports.signOut.subscribe(function() {
+                auth.signOut();
+            });
+
+            // Listen for sign-in state changes and send to elm
+            auth.isSignedIn.listen(function() {
+                sendUser(auth);
+            });
+
+            // Send initial user to elm
+            sendUser(auth);
+        }, function(res) {
+            app.ports.getUserFailed.send(res.details);
+        });
+    });
 });
 
 // Listen for listReminders requests from elm
 app.ports.listReminders.subscribe(function() {
-    listReminders();
+    listReminders().then(function(res) {
+        var items = res.result.items.map(formatReminder);
+        app.ports.listRemindersSuccess.send(items);
+    }, function(res) {
+        app.ports.listRemindersFailed.send(res.result.error.message);
+    });
+});
+
+// Listen for createReminder requests from elm
+app.ports.createReminder.subscribe(function(draft) {
+    createReminder(draft).then(function(res) {
+        var reminder = formatReminder(res.result);
+        app.ports.createReminderSuccess.send(reminder);
+    }, function(res) {
+        app.ports.createReminderFailed.send(res.result.error.message)
+    });
 });
 
 // Listen for parseQuery requests from elm
@@ -16,48 +66,11 @@ app.ports.parseQuery.subscribe(function(query) {
     app.ports.draft.send(draft);
 });
 
-// Listen for createReminder requests from elm
-app.ports.createReminder.subscribe(function(draft) {
-    createReminder(draft);
-});
-
-
-function gapiInit() {
-    gapi.client.init({
-        'apiKey': 'AIzaSyAYk1JO0wu3UxAbYmeDxLclOA2psEwLQZE',
-        'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
-        'clientId': '853474051422-rbvibfhir17apk19nq9vf3j8sshdo81f.apps.googleusercontent.com',
-        'scope': 'https://www.googleapis.com/auth/calendar',
-    }).then(function() {
-        var auth = gapi.auth2.getAuthInstance();
-
-        // Listen for sign in events from elm
-        app.ports.signIn.subscribe(function() {
-            auth.signIn();
-        });
-
-        // Listen for sign out events from elm
-        app.ports.signOut.subscribe(function() {
-            auth.signOut();
-        });
-
-        // Listen for sign-in state changes and send to elm
-        auth.isSignedIn.listen(function() {
-            sendUser(auth);
-        });
-
-        // Send initial user to elm
-        sendUser(auth);
-    }, function(res) {
-        app.ports.getUserFailed.send(res.details);
-    });
-
-}
 
 function listReminders() {
     var now = new Date();
 
-    var req = gapi.client.calendar.events.list({
+    return gapi.client.calendar.events.list({
         calendarId: "primary",
         maxResults: 250,
         timeMin: now.toISOString(),
@@ -65,13 +78,34 @@ function listReminders() {
         singleEvents: true,
         privateExtendedProperty: "isReminder=true"
     });
+}
 
-    // TODO: check response code
-    req.then(function(res) {
-        var items = res.result.items.map(formatReminder);
-        app.ports.listRemindersSuccess.send(items);
-    }, function(res) {
-        app.ports.listRemindersFailed.send(res.result.error.message);
+function createReminder(draft) {
+    var event = {
+        summary: draft.title,
+        start: {
+            dateTime: draft.startDate,
+        },
+        end: {
+            dateTime: draft.endDate,
+        },
+        reminders: {
+            useDefault: false,
+            overrides: [
+                {method: 'email', minutes: 0},
+                {method: 'popup', minutes: 0},
+            ],
+        },
+        extendedProperties: {
+            private: {
+                isReminder: true,
+            },
+        },
+    };
+
+    return gapi.client.calendar.events.insert({
+        'calendarId': 'primary',
+        'resource': event
     });
 }
 
@@ -119,54 +153,11 @@ function parseQuery(query) {
     };
 }
 
-function createReminder(draft) {
-    var event = {
-        summary: draft.title,
-        start: {
-            dateTime: draft.startDate,
-        },
-        end: {
-            dateTime: draft.endDate,
-        },
-        reminders: {
-            useDefault: false,
-            overrides: [
-                {method: 'email', minutes: 0},
-                {method: 'popup', minutes: 0},
-            ],
-        },
-        extendedProperties: {
-            private: {
-                isReminder: true,
-            },
-        },
-    };
-
-    var request = gapi.client.calendar.events.insert({
-        'calendarId': 'primary',
-        'resource': event
-    });
-
-    request.then(function(res) {
-        var reminder = formatReminder(res.result);
-        app.ports.createReminderSuccess.send(reminder);
-    }, function(res) {
-        app.ports.createReminderFailed.send(res.result.error.message)
-    });
-}
-
 function humanDate(dt) {
     return moment(dt).format("HH:mm @ dddd, DD MMM YYYY");
 }
 
-// Helper function to send current user to elm
-function sendUser(auth) {
-    var user = getUser(auth);
-    app.ports.getUserSuccess.send(user);
-}
-
-
-function getUser(auth) {
+function formatUser(auth) {
     var isAuthorized = auth.isSignedIn.get();
 
     if (!isAuthorized) {
